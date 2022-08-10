edd6e4c6928bfe448c071945e745c14ccdf67ccb
Alan Bahena <alanbahena1@gmail.com>
2022-08-10 18:02:01 -0500

    Mailchimp API corrected

--- public/js/mailchimp.js
+++ public/js/mailchimp.js
@@ -1,6 +1,32 @@
-const subscriptionForm = document.querySelector(".form_popup");
+const subscriptionForm = document.querySelector(".form");
+const subscriptionForm2 = document.querySelector(".form_popup");
 
 subscriptionForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let formData = {
+    email: email.value,
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "/subscription");
+  xhr.setRequestHeader("content-type", "application/json");
+
+  xhr.onload = function () {
+    console.log(xhr.responseText);
+    if (xhr.responseText == "success") {
+      alert("Suscripción con exito");
+      email.value = "";
+    } else {
+      alert("Algo ha ocurrido! Por favor volver a intentar");
+    }
+  };
+
+  xhr.send(JSON.stringify(formData));
+  console.log("HEllo from the script");
+});
+
+subscriptionForm2.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let formData = {
