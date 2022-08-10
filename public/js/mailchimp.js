84d94d6d12415aa46a47a9684980a79c292fc2b7
Alan Bahena <alanbahena1@gmail.com>
2022-08-10 18:13:08 -0500

    Mailchimp Test 6

--- public/js/mailchimp.js
+++ public/js/mailchimp.js
@@ -1,25 +1,25 @@
-const subscriptionForm = document.querySelector(".form_popup");
+// const subscriptionForm = document.querySelector(".form_popup");
 
-subscriptionForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+// subscriptionForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  let formData = {
-    email: email.value,
-  };
+//   let formData = {
+//     email: email.value,
+//   };
 
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "/subscription");
-  xhr.setRequestHeader("content-type", "application/json");
+//   let xhr = new XMLHttpRequest();
+//   xhr.open("POST", "/subscription");
+//   xhr.setRequestHeader("content-type", "application/json");
 
-  xhr.onload = function () {
-    console.log(xhr.responseText);
-    if (xhr.responseText == "success") {
-      alert("Suscripción con exito");
-      email.value = "";
-    } else {
-      alert("Algo ha ocurrido! Por favor volver a intentar");
-    }
-  };
+//   xhr.onload = function () {
+//     console.log(xhr.responseText);
+//     if (xhr.responseText == "success") {
+//       alert("Suscripción con exito");
+//       email.value = "";
+//     } else {
+//       alert("Algo ha ocurrido! Por favor volver a intentar");
+//     }
+//   };
 
-  xhr.send(JSON.stringify(formData));
-});
+//   xhr.send(JSON.stringify(formData));
+// });
