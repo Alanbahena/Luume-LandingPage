f97e088c05a54a39636b2773aa70092bc6f5edcb
Alan Bahena <alanbahena1@gmail.com>
2022-08-10 18:10:32 -0500

    Mailchimp Test 5

--- public/js/mailchimp.js
+++ public/js/mailchimp.js
@@ -1,5 +1,4 @@
-const subscriptionForm = document.querySelector(".form");
-const subscriptionForm2 = document.querySelector(".form_popup");
+const subscriptionForm = document.querySelector(".form_popup");
 
 subscriptionForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -23,30 +22,4 @@ subscriptionForm.addEventListener("submit", (e) => {
   };
 
   xhr.send(JSON.stringify(formData));
-  console.log("HEllo from the script");
-});
-
-subscriptionForm2.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  let formData = {
-    email: email.value,
-  };
-
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "/subscription2");
-  xhr.setRequestHeader("content-type", "application/json");
-
-  xhr.onload = function () {
-    console.log(xhr.responseText);
-    if (xhr.responseText == "success") {
-      alert("Suscripción con exito");
-      email.value = "";
-    } else {
-      alert("Algo ha ocurrido! Por favor volver a intentar");
-    }
-  };
-
-  xhr.send(JSON.stringify(formData));
-  console.log("HEllo from the script");
 });
